ad6aabf5ce72af2a6278f980201fcc6f8ad06fe5
Xin Qiao <qiaoxin136@gmail.com>
2025-02-21 15:17:27 -0500

    add data

--- src/App.tsx
+++ src/App.tsx
@@ -17,22 +17,21 @@ function App() {
     client.models.Todo.create({ content: window.prompt("Todo content") });
   }
 
+    
+  function deleteTodo(id: string) {
+    client.models.Todo.delete({ id })
+  }
+
   return (
     <main>
       <h1>My todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
-          <li key={todo.id}>{todo.content}</li>
+          <li key={todo.id}          onClick={() => deleteTodo(todo.id)}>{todo.content}</li>
         ))}
       </ul>
-      <div>
-        🥳 App successfully hosted. Try creating a new todo.
-        <br />
-        <a href="https://docs.amplify.aws/react/start/quickstart/#make-frontend-updates">
-          Review next step of this tutorial.
-        </a>
-      </div>
+
     </main>
   );
 }
