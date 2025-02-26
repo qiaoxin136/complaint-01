9604db221c1545f25cdda06594578a8fea678441
qiaoxin136 <qiaoxin136@gmail.com>
2025-02-26 10:58:47 -0500

    add geo

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -18,7 +18,7 @@ const geoStack = backend.createStack("geo-stack");
 
 // create a location services map
 const map = new CfnMap(geoStack, "Map", {
-  mapName: "myMap",
+  mapName: "myMap02",
   description: "Map",
   configuration: {
     style: "VectorEsriNavigation",
@@ -27,7 +27,7 @@ const map = new CfnMap(geoStack, "Map", {
   tags: [
     {
       key: "name",
-      value: "myMap",
+      value: "myMap02",
     },
   ],
 });
